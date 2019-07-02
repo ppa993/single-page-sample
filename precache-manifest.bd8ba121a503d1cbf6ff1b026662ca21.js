bb4ffa4ec1ce665c03dc050347b3a366a4e897f8
An Pham Phuoc <ppa.993@gmail.com>
2019-07-02 17:11:33 +0700

    Updates

---
+++ precache-manifest.bd8ba121a503d1cbf6ff1b026662ca21.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43306ef1d307946e9526a261949002b",
+    "url": "/single-page-sample/index.html"
+  },
+  {
+    "revision": "27094aed7542c0d172ba",
+    "url": "/single-page-sample/static/js/2.c5db40b9.chunk.js"
+  },
+  {
+    "revision": "db2993d86cd1804493f3",
+    "url": "/single-page-sample/static/js/main.95a11b55.chunk.js"
+  },
+  {
+    "revision": "5e4826e2d58f822ea17a",
+    "url": "/single-page-sample/static/js/runtime~main.2a61ea42.js"
+  }
+]);
